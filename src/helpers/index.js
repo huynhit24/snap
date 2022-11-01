fe5fd4ba0a05136f8bb5f2c263810c102b1d9ee0
Huỳnh <huynh.it.24@gmail.com>
2022-11-01 17:04:50 +0700

    feat: add helper for theme

---
+++ src/helpers/index.js
@@ -0,0 +1,37 @@
+import { COLOR_VAR_KEYS, THEME_KEYS} from 'constants';
+const htmlRoot = document.querySelector(':root');
+
+// get current root palettes
+function getRootPalettes() {
+    let palettes = [];
+    const rootStyles = getComputedStyle(htmlRoot);
+  
+    COLOR_VAR_KEYS.forEach((item) => {
+      let color = rootStyles.getPropertyValue(item.key);
+  
+      palettes.push({
+        ...item,
+        color: color[0] === '#' ? color : color.slice(1), // output ' #123456' => slice(1) = '#123456'
+      });
+    });
+  
+    return palettes;
+}
+
+// get custom palettes from local storage
+export const getCustomPalettes = () => {
+    const palettes = localStorage.getItem(THEME_KEYS.PALETTE_KEY);
+    if (!palettes) {
+      const newPalettes = getRootPalettes();
+      localStorage.setItem(THEME_KEYS.PALETTE_KEY, JSON.stringify(newPalettes));
+      return newPalettes;
+    } else {
+      return JSON.parse(palettes);
+    }
+};
+
+// set palettes for root
+export const setRootPalettes = () => {
+    const palettes = getCustomPalettes();
+    palettes?.forEach((item) => htmlRoot.style.setProperty(item.key, item.color));
+};
